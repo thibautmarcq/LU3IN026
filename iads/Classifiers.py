ed9bcf6e034e9a0a0cdf9eff050d24d7d27e2225
Thibaut M <thibaut.marcq@gmail.com>
2025-02-10 11:03:47 +0100

    Suite TME 03 et import TME 04 + iads

---
+++ iads/Classifiers.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package: iads
+File: Classifiers.py
+Année: LU3IN026 - semestre 2 - 2024-2025, Sorbonne Université
+"""
+
+# Classfieurs implémentés en LU3IN026
+# Version de départ : Février 2025
+
+# Import de packages externes
+import numpy as np
+import pandas as pd
+
+# ---------------------------
---
+++ iads/utils.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package: iads
+File: utils.py
+Année: LU3IN026 - semestre 2 - 2024-2025, Sorbonne Université
+"""
+
+
+# Fonctions utiles
+# Version de départ : Février 2025
+
+# import externe
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# ------------------------ 
